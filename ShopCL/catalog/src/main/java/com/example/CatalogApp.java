6568ce5018180b97409384c93a3a3461e5e949be
Andrew.p52 <Andrew.p52@yandex.ru>
2023-04-02 19:32:09 +0300

    some services mods

--- ShopCL/catalog/src/main/java/com/example/CatalogApp.java
+++ ShopCL/catalog/src/main/java/com/example/CatalogApp.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MailerApp {
+public class CatalogApp {
     public static void main(String[] args) {
-        SpringApplication.run(MailerApp.class, args);
+        SpringApplication.run(CatalogApp.class, args);
     }
 }
